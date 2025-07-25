c4983db723e5073bcc024bd6ed919c93e3d80628
vaishnavi <deshmukhvaishnavi1907@gmail.com>
2025-07-25 22:29:27 +0530

    Assignment3 solved

---
+++ assignment3/task2.py
@@ -0,0 +1,14 @@
+import math
+
+# Ask user for input
+num = float(input("Enter a number: "))
+
+# Perform calculations
+sqrt = math.sqrt(num)
+log = math.log(num)
+sine = math.sin(num)
+
+# Display results
+print(f"Square root of {num} is: {sqrt}")
+print(f"Natural log (ln) of {num} is: {log}")
+print(f"Sine of {num} radians is: {sine}")
